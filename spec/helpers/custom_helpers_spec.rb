01bc909255600e6e59cb4f30987599fd765060ee
Joshua Steele <joshukraine@gmail.com>
2016-05-27 12:21:49 +0300

    Update tests with info specific to ETO site.

--- spec/helpers/custom_helpers_spec.rb
+++ spec/helpers/custom_helpers_spec.rb
@@ -17,24 +17,6 @@ describe CustomHelpers do
     end
   end
 
-  describe "#ministries_smart_path" do
-    context "when on home page" do
-      it "returns the Ministries path without a leading slash" do
-        current_page = double("Sitemap")
-        allow(current_page).to receive(:path).and_return("index")
-        expect(ministries_smart_path(current_page.path)).to eq("#ministries")
-      end
-    end
-
-    context "when not on home page" do
-      it "returns the Ministries path *with* a leading slash" do
-        current_page = double("Sitemap")
-        allow(current_page).to receive(:path).and_return("other-page")
-        expect(ministries_smart_path(current_page.path)).to eq("/#ministries")
-      end
-    end
-  end
-
   describe "#home_smart_path" do
     context "when on home page" do
       it "returns '#'" do
@@ -49,6 +31,24 @@ describe CustomHelpers do
         current_page = double("Sitemap")
         allow(current_page).to receive(:path).and_return("other-page")
         expect(home_smart_path(current_page.path)).to eq("/")
+      end
+    end
+  end
+
+  describe "#ministries_smart_path" do
+    context "when on home page" do
+      it "returns the Ministries path without a leading slash" do
+        current_page = double("Sitemap")
+        allow(current_page).to receive(:path).and_return("index")
+        expect(ministries_smart_path(current_page.path)).to eq("#ministries")
+      end
+    end
+
+    context "when not on home page" do
+      it "returns the Ministries path *with* a leading slash" do
+        current_page = double("Sitemap")
+        allow(current_page).to receive(:path).and_return("other-page")
+        expect(ministries_smart_path(current_page.path)).to eq("/#ministries")
       end
     end
   end
