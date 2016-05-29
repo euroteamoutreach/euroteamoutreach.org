22a614c308200ccd1110a90adf389c8b6c628709
Joshua Steele <joshukraine@gmail.com>
2016-05-29 12:15:21 +0300

    Test for noindex/nofollow robot meta tags

--- spec/features/contact_thanks_spec.rb
+++ spec/features/contact_thanks_spec.rb
@@ -6,4 +6,9 @@ describe "Contact thank you page", type: :feature do
   it "displays the correct thank you message" do
     expect(page).to have_selector("p", text: "Your message has been sent.")
   end
+
+  it "displays 'noindex, nofollow' for robots" do
+    meta_tag = "meta[name=\"robots\"][content=\"noindex, nofollow\"]"
+    expect(page).to have_css(meta_tag, visible: false)
+  end
 end
