2f245d65147b6499253f10942931b4898315405a
Joshua Steele <joshukraine@gmail.com>
2018-10-23 19:38:12 -0500

    Update tests for Sargent bio page.

--- spec/features/beal_spec.rb
+++
@@ -1,9 +0,0 @@
-describe "Beal bio page", type: :feature do
-  before do
-    visit "/beal"
-  end
-
-  it "displays the correct heading" do
-    expect(page).to have_selector("h1", text: "The Beal Family")
-  end
-end
